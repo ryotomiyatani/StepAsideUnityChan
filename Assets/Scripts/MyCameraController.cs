864a13972b3efac0981687a9d766983a1a7cc84a
miyatani <mmmiyan009@gmail.com>
2017-08-12 12:38:16 +0900

    3Dゲームアプリを作ろう Vol2
    StepAsideUnityChanプロジェクトの作成

---
+++ Assets/Scripts/MyCameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCameraController : MonoBehaviour {
+	//Unityちゃんのオブジェクト
+	private GameObject unitychan;
+	//Unityちゃんとのカメラ位置
+	private float difference;
+	// Use this for initialization
+	void Start () {
+		//Unityちゃんのオブジェクトを取得
+		this.unitychan = GameObject.Find("unitychan");
+		//Unityちゃんとのカメラの位置(z軸)の差を求める
+		this.difference = unitychan.transform.position.z - this.transform.position.z;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3(0, this.transform.position.y, this.unitychan.transform.position.z - difference);
+	}
+}
